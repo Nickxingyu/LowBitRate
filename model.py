2fc03fa6c8fa683f5f8a7859da75ceb992b26892
Nero <nick19960723@gmail.com>
2023-12-26 13:41:19 +0800

    feat: get the mask for spatial prior

--- model.py
+++ model.py
@@ -12,20 +12,96 @@ print(model.g_a.state_dict())
 
 class MeanScaleHalfHyperprior(MeanScaleHyperprior):
     def __init__(self, N, M, **kwargs):
-        super.__init__(N=N, M=M, **kwargs
+        N = int(N)
+        M = int(M)
+        if M % 2 != 0:
+            raise ValueError(f"Num of channels of feature map have to be even.")
+        super().__init__(N=N, M=M, **kwargs)
+        self.N = N
+        self.M = M
 
-        self.h_a = nn.Sequential(
-            conv(M, N, stride=1, kernel_size=3),
-            nn.LeakyReLU(inplace=True),
-            conv(N, N),
-            nn.LeakyReLU(inplace=True),
-            conv(N, N),
-        )
+    def forward(self, x):
+        # print(x.shape)
+        
+        # y = self.g_a(x)
+        # z = self.h_a(y)
+        # z_hat, z_likelihoods = self.entropy_bottleneck(z)
+        # gaussian_params = self.h_s(z_hat)
+        # scales_hat, means_hat = gaussian_params.chunk(2, 1)
 
-        self.h_s = nn.Sequential(
-            deconv(N, M),
-            nn.LeakyReLU(inplace=True),
-            deconv(M, M * 3 // 2),
-            nn.LeakyReLU(inplace=True),
-            conv(M * 3 // 2, M * 3, stride=1, kernel_size=3),
-        )
+        ## AE mean parameters
+        #  split means_hat and scales_hat for 
+        #  encoding and decoding feature map
+
+
+        ## Directly predictive parameters
+        #  combine the decoded feature map and the predictive 
+        #  feature map, and push it into the context model for
+        #  constructing whole feature map.
+
+    @staticmethod
+    def get_mask(height, width, dtype, device):
+        micro_mask = torch.tensor(((1, 0), (0, 1)), dtype=dtype, device=device)
+        micro_row = torch.tensor((1, 0), dtype=dtype, device=device)
+        mask_row = micro_row.repeat(height // 2)
+        mask_col = micro_row.repeat(width // 2)
+        mask_0 = micro_mask.repeat(height // 2, width // 2)
+
+        
+        # make the mask for odd num of rows or cols
+        if height % 2 == 1 and width % 2 == 1:
+            mask_col = torch.cat((mask_col, torch.tensor([1], dtype=dtype, device=device)), dim=0)
+
+        if width % 2 == 1:
+            mask_0 = torch.cat((mask_0, torch.unsqueeze(mask_row, 1)), dim=1)
+            
+        if height % 2 == 1:
+            mask_0 = torch.cat((mask_0, torch.unsqueeze(mask_col, 0)), dim=0)
+            
+        
+        mask_0 = torch.unsqueeze(mask_0, 0)
+        mask_0 = torch.unsqueeze(mask_0, 0)
+        mask_1 = torch.ones_like(mask_0) - mask_0
+        return mask_0, mask_1
+
+    """
+    Process with mask
+
+    def example(self, x):
+        dtype = x.dtype
+        device = x.device
+        _, _, H, W = x.size()
+        mask_0, mask_1 = self.get_mask(H, W, dtype, device)
+
+    
+    mask_0.shape : torch.Size([1, 1, H, W])
+    mask_1.shape : torch.Size([1, 1, H, W])
+
+    mask_0 with H=8, W=8:
+
+    [[[
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+    ]]]
+
+    mask_1 with H=8, W=8:
+
+    [[[
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+    ]]]
+    
+    """
+
--- train.py
+++ train.py
@@ -2,8 +2,10 @@ import sys
 import argparse
 
 from torchvision import transforms
+from torch.utils.data import DataLoader
 
 from dataset import V90kList
+from model import MeanScaleHalfHyperprior
 
 def parse_args(argv: list[str]):
     parser = argparse.ArgumentParser()
@@ -18,19 +20,53 @@ def parse_args(argv: list[str]):
         "--patch-size",
         type=int,
         nargs=2,
-        default=(256, 256),
+        default=(7, 7),
         help="Size of the patches to be cropped (default: %(default)s)",
+    )
+
+    parser.add_argument(
+        "-n",
+        "--num-workers",
+        type=int,
+        default=4,
+        help="Dataloaders threads (default: %(default)s)",
+    )
+
+    parser.add_argument(
+        "--batch-size", 
+        type=int, 
+        default=16, 
+        help="Batch size (default: %(default)s)",
     )
 
     return parser.parse_args(argv)
 
 def main(argv):
     args = parse_args(argv)
+    device = "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
+    
     train_transform = transforms.Compose(
         [transforms.RandomCrop(args.patch_size), transforms.ToTensor()]
     )
 
     train_dataset = V90kList("../data/train/", transform=train_transform)
+
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        shuffle=True,
+        pin_memory=(device == "cuda"),
+    )
+
+    model = MeanScaleHalfHyperprior(128, 192)
+    
+    print(train_dataset[0].shape)
+    for i, d in enumerate(train_dataloader):
+        print(d.shape)
+        output = model(d)
+        break
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
