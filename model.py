ab88300fa5d0c427ed44c4c5e343fd0d73189cb5
Hero <nick19960723@gmail.com>
2024-01-05 13:57:30 +0800

    feat: spatially merge and predict latent feature map

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import torch
 
+from torch import nn
 from compressai.models import MeanScaleHyperprior
+from compressai.models.utils import conv
 
 """
 model = MeanScaleHyperprior(128, 192)
@@ -15,53 +17,78 @@ class MeanScaleHalfHyperprior(MeanScaleHyperprior):
     def __init__(self, N, M, **kwargs):
         N = int(N)
         M = int(M)
-        if M % 2 != 0:
+        if N % 2 != 0 or M % 2 != 0:
             raise ValueError(f"Num of channels of feature map have to be even.")
         super().__init__(N=N, M=M, **kwargs)
         self.N = N
         self.M = M
+        self.e_p = nn.Sequential(
+            conv(M * 2, M * 2, stride=1, kernel_size=3),
+        )
+
+        self.y_predictor = nn.Sequential(
+            conv(M * 4, M * 2, stride=1, kernel_size=3),
+            nn.LeakyReLU(inplace=True),
+            conv(M * 2, M * 1, stride=1, kernel_size=3),
+            nn.LeakyReLU(inplace=True),
+            conv(M * 1, M * 1, stride=1, kernel_size=3),
+        )
 
     def forward(self, x):
         y = self.g_a(x)
         z = self.h_a(y)
         z_hat, z_likelihoods = self.entropy_bottleneck(z)
 
-        gaussian_params = self.h_s(z_hat)
+        hyperprior = self.h_s(z_hat)
+        gaussian_params = self.e_p(hyperprior)
         scales_hat, means_hat = gaussian_params.chunk(2, 1)
 
         y_0, _ = self.spatial_split(y)
         scales_hat_0, scales_hat_1 = self.spatial_split(scales_hat)
         means_hat_0, means_hat_1 = self.spatial_split(means_hat)
 
-        y_0_hat, y_0_likelihood = self.gaussian_conditional(
+        y_0_hat, y_0_likelihoods = self.gaussian_conditional(
             y_0,
             scales_hat_0,
             means=means_hat_0,
         )
 
+        means_hat_0 = y_0_hat
+        means_hat = self.spatial_merge(means_hat_0, means_hat_1)
+        scales_hat_0 = torch.zeros_like(scales_hat_0)
+        scales_hat = self.spatial_merge(scales_hat_0, scales_hat_1)
+        param_for_prediction = torch.cat((means_hat, scales_hat, hyperprior), dim=1)
+
+        y_predicted = self.y_predictor(param_for_prediction)
+        x_hat = self.g_s(y_predicted)
+
         return {
-            z_hat,
+            x_hat,
+            y_0_likelihoods,
+            z_likelihoods,
         }
-        ## AE mean parameters
-        #  split means_hat and scales_hat for
-        #  encoding and decoding feature map
 
-        ## Directly predictive parameters
-        #  combine the decoded feature map and the predictive
-        #  feature map, and push it into the context model for
-        #  constructing whole feature map.
-
-    def spatial_split(self, y):
-        dtype = y.dtype
-        device = y.device
-        _, _, H, W = y.size()
+    def spatial_split(self, feature_map):
+        dtype = feature_map.dtype
+        device = feature_map.device
+        _, _, H, W = feature_map.size()
         mask_0, mask_1 = self.get_mask(H, W, dtype, device)
 
-        y_0, y_1 = y.chunk(2, 1)
-        y_0_0, y_1_0 = y_0 * mask_0, y_1 * mask_0
-        y_0_1, y_1_1 = y_0 * mask_1, y_1 * mask_1
+        feature_map_0, feature_map_1 = feature_map.chunk(2, 1)
+        feature_map_0_0, feature_map_1_0 = (
+            feature_map_0 * mask_0,
+            feature_map_1 * mask_0,
+        )
+        feature_map_0_1, feature_map_1_1 = (
+            feature_map_0 * mask_1,
+            feature_map_1 * mask_1,
+        )
 
-        return y_0_0 + y_1_1, y_0_1 + y_1_0
+        return feature_map_0_0 + feature_map_1_1, feature_map_0_1 + feature_map_1_0
+
+    def spatial_merge(self, feature_map_0, feature_map_1):
+        feature_map = torch.cat((feature_map_0, feature_map_1), dim=1)
+        return torch.cat(self.spatial_split(feature_map), dim=1)
 
     @staticmethod
     def get_mask(height, width, dtype, device):
